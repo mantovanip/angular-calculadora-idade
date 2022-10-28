b376bc93a6c12fc738582420fe50dc3f05e00628
mantovanip <75042009+mantovanip@users.noreply.github.com>
2022-10-28 12:09:25 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public ano_nasc: number = 0;
+  public data_nasc: number = 0;
   public result: number = 0;
   public result_month: number = 0;
   public result_days: number = 0;
 
   calcular() {
-    this.result = 2022 - this.ano_nasc;
+    this.result = 2022 - this.data_nasc;
     this.result_month = this.result * 12;
-    this.result_days = this.result * 365;
+    this.result_days = this.result_month * 365;
   }
 }
